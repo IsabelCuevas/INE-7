3f51f158ff3b769308c3891b34d9922043bea29d
JuanCortes96 <31145548+JuanCortes96@users.noreply.github.com>
2018-03-07 22:55:32 -0600

    Add files via upload
    Primera base del programa, no tiene nada incluido, solo es para dar una idea de como funcionara

---
+++ main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include "menu.h"
+
+int main(){;
+    int opcion, interfaz;
+  do{
+     interfaz = crearMenu("\nElija la opcion que desee\n1)Votar\n2)Revisar estadisticas\n", 2);
+    switch(interfaz){
+     case 1: //votar
+        break;
+     case 2://Revisar estadisticas
+	    do{
+		opcion = crearMenu("\nElija la opcion que desee\n1)Insertar\n2)Listar\n3)Actualizar\n4)Borrar\n5)Buscar\n6)Salir\n", 6);
+		switch(opcion){
+
+	    case 1: //insertar
+	    break;
+
+		case 2: //listar
+	    break;
+
+		case 3: //actualizar
+	    break;
+
+		case 4: //borrar
+	    break;
+
+		case 5: //buscar
+	    break;
+
+		case 6: //salir
+	    break;
+	    }
+
+	    }while(opcion>0&&opcion<6);
+        break;
+      }
+	}while(interfaz>0&&interfaz<3);
+	return 0;
+}
